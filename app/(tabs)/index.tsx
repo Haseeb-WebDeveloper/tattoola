9330935840529fd8d85d24db34f6f1168b45d3e7
Haseeb Ahmed Raza Khan <web.dev.haseeb@gmail.com>
2025-09-25 13:38:57 +0500

    setup done

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+ 
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!AHDJAKD Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ab itaque commodi consequatur facilis quibusdam aliquam laborum vel nam incidunt excepturi quae repudiandae, ea corporis sit vero alias? Atque, cupiditate?
+      </Text>
+    </View>
+  );
+}
