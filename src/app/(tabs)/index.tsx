cf95af69e26aba1d329868f4640df9429e92f786
Haseeb Ahmed Raza Khan <web.dev.haseeb@gmail.com>
2025-10-05 09:33:08 +0500

    checking if test apk is working

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-foreground">Home</Text>
+      <Text className="text-xl font-bold text-foreground">Home screen (after update)</Text>
     </View>
   );
 }
