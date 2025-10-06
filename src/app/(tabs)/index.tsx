305c8fec86453a2135a075af0a002dd1af3106ef
Haseeb Ahmed Raza Khan <web.dev.haseeb@gmail.com>
2025-10-06 10:33:13 +0500

    updated UI or bug fix

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-foreground">Home screen (after update)</Text>
+      <Text className="text-xl font-bold text-foreground">Home screen (2nd update)</Text>
     </View>
   );
 }
